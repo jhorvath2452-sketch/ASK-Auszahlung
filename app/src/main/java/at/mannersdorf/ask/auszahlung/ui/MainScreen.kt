aada142cb62b4cd91bd0e2ed82f9882e75445644
chigo2452 <j.horvath2452@gmail.com>
2026-09-20 22:05:58 +0200

    ExposedDropdownMenu-Import entfernt

--- app/src/main/java/at/mannersdorf/ask/auszahlung/ui/MainScreen.kt
+++ app/src/main/java/at/mannersdorf/ask/auszahlung/ui/MainScreen.kt
@@ -10,9 +10,6 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenu
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
